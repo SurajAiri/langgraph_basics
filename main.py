18e6f280db60f4e9a9cc9cad921d8a49021efc12
SurajAiri <surajairi.ml@gmail.com>
2025-06-22 23:20:15 +0530

    test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from langgraph-basics!")
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 
+
+def main():
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0.4)
+    res = llm.invoke("What is the capital of France?")
+    print(res.content)
 
 if __name__ == "__main__":
     main()
